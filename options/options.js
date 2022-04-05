278d109d2f44b1c0b338b8cd7985a6ceb019d7c7
Crowdbotics <team@crowdbotics.com>
2022-04-05 22:45:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cherish_towing_34614",
+  url: "https://cherish_towing_34614.botics.co",
+  api: "https://cherish_towing_34614.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
